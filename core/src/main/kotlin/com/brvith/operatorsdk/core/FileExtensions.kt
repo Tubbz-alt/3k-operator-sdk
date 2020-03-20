4b24595437726c40e93eba59f5ee6ee10f0f98c0
Brinda Santh <brindasanth@gmail.com>
2020-03-19 21:10:16 -0400

    Add Licence headers.

--- core/src/main/kotlin/com/brvith/operatorsdk/core/FileExtensions.kt
+++ core/src/main/kotlin/com/brvith/operatorsdk/core/FileExtensions.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2020 Brvith Solutions.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.brvith.operatorsdk.core
 
 import io.kubernetes.client.openapi.models.V1ServiceAccount
@@ -14,6 +30,10 @@ import kotlin.reflect.KClass
 fun <T : Any> logger(clazz: T) = LoggerFactory.getLogger(clazz.javaClass)!!
 
 fun <T : KClass<*>> logger(clazz: T) = LoggerFactory.getLogger(clazz.java)!!
+
+fun mainResourceFile(path: String, vararg more: String?): File {
+    return Paths.get("./src/main/resources/$path", *more).toFile().normalize()
+}
 
 fun testResourceFile(path: String, vararg more: String?): File {
     return Paths.get("./src/test/resources/$path", *more).toFile().normalize()
